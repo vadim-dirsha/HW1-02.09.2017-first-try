a792ba129e4b141567e1f7fc6cc579ec0435fda4
ZedZul <faqproshival@yandex.ru>
2017-10-03 00:52:08 +0300

    initial setup

---
+++ app/src/main/java/com/zedzul/github/hw1_02092017/MainActivity.java
@@ -0,0 +1,13 @@
+package com.zedzul.github.hw1_02092017;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
