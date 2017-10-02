6c85202fa07c6afaabbbfcac8cef0ef66c21e72d
ZedZul <faqproshival@yandex.ru>
2017-10-03 01:59:00 +0300

    Some changes

--- app/src/main/java/com/zedzul/github/hw1_02092017/MainActivity.java
+++ app/src/main/java/com/zedzul/github/hw1_02092017/MainActivity.java
@@ -1,12 +1,12 @@
 package com.zedzul.github.hw1_02092017;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
