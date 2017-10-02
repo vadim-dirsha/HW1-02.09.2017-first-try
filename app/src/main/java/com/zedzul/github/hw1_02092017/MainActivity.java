e3b30e077c7bf672236e41e00a778ce927abbb1d
ZedZul <faqproshival@yandex.ru>
2017-10-03 02:52:26 +0300

    add feature_1

--- app/src/main/java/com/zedzul/github/hw1_02092017/MainActivity.java
+++ app/src/main/java/com/zedzul/github/hw1_02092017/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //feature_1
     }
 }
