7fe3e3041c21ace404ad67d515dbfd28d9bca50a
ZedZul <faqproshival@yandex.ru>
2017-10-03 03:04:59 +0300

    good log added

--- app/src/main/java/com/zedzul/github/hw1_02092017/MainActivity.java
+++ app/src/main/java/com/zedzul/github/hw1_02092017/MainActivity.java
@@ -2,8 +2,12 @@ package com.zedzul.github.hw1_02092017;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG_BAD = "BAD";
+    public static final String MSG_FATAL_ERROR = "FATAL ERROR";
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -11,5 +15,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //feature_n
         //feature_1
+        Log.d(TAG_BAD, MSG_FATAL_ERROR);
     }
 }
